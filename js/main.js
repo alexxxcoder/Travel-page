e5c398174802eb67b4f20e1792283313a625d1e3
Alex <alexxxcoder@gmail.com>
2024-05-23 17:52:01 +0200

    the header is ready

--- js/main.js
+++ js/main.js
@@ -7,85 +7,42 @@ document.querySelector('.burger-icon').addEventListener('click', function () {
 
 //////////////////// SCROLLTRIGGER  ///////////
 
-
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.to(".header__menu", {
+    x: 1000,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".burger-icon",
+        start: "top 0%",
+        scrub: 2,
+        // markers: true,
+    },
+});
 
-
-// gsap.to(".header__menu", {
-//     x: 1000,
-//     duration: 3,
-//     scrollTrigger: {
-//         trigger: ".header__menu",
-//         start: "top 0%",
-//         scrub: 2,
-//         markers: true,
-//     },
-
-// });
-
-// // Анімація логотипу
-// gsap.to(".header__logo", {
-//     x: -1000, // переміщення у протилежний бік
-//     duration: 8,
-//     scrollTrigger: {
-//         trigger: ".header__logo",
-//         start: "top 0%",
-//         // end: "top 20%",
-//         scrub: 2,
-//         markers: true,
-//     }
-// });
-
-
-function turnOnGsapAnimation() {
-    const screenWidth = window.innerWidth;
-    const targetWidth = 900;
-
-    if (2 > 1) {
-
-        gsap.to(".header__menu", {
-            x: 1000,
-            duration: 3,
-            scrollTrigger: {
-                trigger: ".header__menu",
-                start: "top 0%",
-                scrub: 2,
-                markers: true,
-            },
-
-        });
 ///// animation burger-icon  //////
-        gsap.to(".burger-icon", {
-            x: 1000,
-            duration: 3,
-            scrollTrigger: {
-                trigger: ".burger-icon",
-                start: "top 0%",
-                scrub: 1,
-                // markers: true,
-            },
 
-        });
-
-        
+gsap.to(".burger-icon", {
+    x: 1000,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".burger-icon",
+        start: "top 0%",
+        scrub: 2,
+        // markers: true,
+    },
+});
 
 ////////    animation header__logo ////////
-        gsap.to(".header__logo", {
-            x: -1000, 
-            duration: 8,
-            scrollTrigger: {
-                trigger: ".header__logo",
-                start: "top 0%",
-                scrub: 1,
-                // markers: true,
-            }
-        });
+
+gsap.to(".header__logo", {
+    x: -1000, 
+    duration: 8,
+    scrollTrigger: {
+        trigger: ".header__logo",
+        start: "top 0%",
+        scrub: 1,
+        // markers: true,
     }
-};
+});
 
-// Виконати функцію під час завантаження сторінки
-window.addEventListener('load', turnOnGsapAnimation);
-
-// Виконати функцію при зміні розміру вікна
-window.addEventListener('resize', turnOnGsapAnimation);
